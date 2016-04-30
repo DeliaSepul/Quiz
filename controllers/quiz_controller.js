2f7ad1ffd34581c9f095982f327c75d9ef418056
DeliaSepul <delia.sepulveda.94@gmail.com>
2016-04-30 18:52:43 +0200

    Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,41 +1,42 @@
 var models = require('../models');
 
-//GET /question
-exports.question = function(req, res, next){
+//GET /quizzes
+exports.index = function(req, res, next){
 	models
 	.Quiz
-	.findOne() //Busca la primera pregunta
-	.then(function(quiz) {
-		if(quiz) {
-			var answer = req.query.answer || '';
-			res.render('quizzes/question', {question: quiz.question, answer: answer});
-		}
-		else {
-			throw new Error('No hay preguntas en la BBDD');
-		}
+	.findAll() //Busca la primera pregunta
+	.then(function(quizzes) {
+		res.render('quizzes/index.ejs', {quizzes: quizzes});
 	}).catch(function(error) { next(error); });
 };
 
-//GET /check
+//GET /quizzes/:id
+exports.show = function(req, res, next){
+	models
+	.Quiz
+	.findById(req.params.quizId) //Busca la primera pregunta
+	.then(function(quiz) {
+		if(quiz){
+			var answer = req.query.answer || '';
+			res.render('quizzes/show', {quiz: quiz, answer: answer});
+		} else {throw new Error('No existe ese quiz en la BBDD'); }
+		
+	}).catch(function(error) { next(error); });
+};
+
+
+//GET /quizzes/:id
 exports.check = function(req, res, next){
 	models
 	.Quiz
-	.findOne() //Busca la primera pregunta
+	.findById(req.params.quizId) //Busca la primera pregunta
 	.then(function(quiz) {
-		if(quiz) {
+		if(quiz){
 			var answer = req.query.answer || '';
-			var result = answer === quiz.answer  ? 'Correcta' : 'Incorrecta';
-			res.render('quizzes/result', {result: result, answer: answer });
-		}
-		else {
-			throw new Error('No hay preguntas en la BBDD');
-		}
+			var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
+			res.render('quizzes/result', {quiz: quiz, result: result, answer: answer});
+		} else {throw new Error('No existe ese quiz en la BBDD'); }
+		
 	}).catch(function(error) { next(error); });
 };
 
-
-
-//GET /author
-exports.author = function(req, res, next){
-	res.render('quizzes/author');
-};
