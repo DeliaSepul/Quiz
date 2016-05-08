5dce446adf8885e261507a0d958c5e976e8b8899
DeliaSepul <delia.sepulveda.94@gmail.com>
2016-05-08 11:54:36 +0200

    Buscador

--- models/index.js
+++ models/index.js
@@ -4,8 +4,8 @@ var path = require('path');
 var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite:
-DATABASE_URL = sqlite:///
-DATABASE_STORAGE = quiz.sqlite
+//DATABASE_URL = sqlite:///
+//DATABASE_STORAGE = quiz.sqlite
 //Usar BBDD Postgres
 //DATABASE_URL = postgres://ewxwdzbktsiedb:f3ohX3A-ABn9w2SKU4aicZOFOK@ec2-174-129-18-170.compute-1.amazonaws.com:5432/d3cpu76fi9fgps
 
@@ -24,7 +24,6 @@ var sequelize = new Sequelize(url, {storage: storage, omitNull: true});
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
-<<<<<<< HEAD
 sequelize.sync()
 .then(function() { //sync() crea la tabla quiz
 	return Quiz.count()
@@ -33,21 +32,6 @@ sequelize.sync()
 				return Quiz.bulkCreate([ {question: 'Capital de Italia', answer: 'Roma'},
 								         {question: 'Capital de Portugal', answer: 'Lisboa'}
 									])
-=======
-sequelize
-.sync()
-.then(function() { //sync() crea la tabla quiz
-	return
-		Quiz
-		.count()
-		.then(function (c) {
-			if (c === 0){ //la tabla se inicializa si está vacía
-				return
-					Quiz
-					.bulkCreate([ {question: 'Capital de Italia', answer: 'Roma'},
-								  {question: 'Capital de Portugal', answer: 'Lisboa'}
-								])
->>>>>>> d417748fa44979bdefc0067bb824e81958ec24e3
 					.then(function() {
 						console.log('Base de datos inicializada con datos');
 					});
